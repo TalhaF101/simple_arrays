40f632d91255e93b1d1cc7eb786327a4e8f83900
Talha <Talha@HP-PAVILION>
2023-10-26 11:41:08 +0500

    Add project files.

---
+++ practice2.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int my_arr[4] = { 14, 12, 78, 69 };
+	int i;
+
+	for ( i = 0; i < 4; i++)
+	{
+		cout << " The value of array " << i << " is = " << my_arr[i] << endl;
+	}
+
+
+
+		return 0;
+}
+
